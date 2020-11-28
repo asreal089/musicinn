ddb6844597872aa9bc0032640a1ff0ae30c98eda
asreal089 <lexx_89@hotmail.com>
2020-11-28 15:09:02 -0300

    commit inical do projeto

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MusicINN - a casa de shows online</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1> olar nosso sistema de musicas</h1>
+      </main>
+
+      <footer className={styles.footer}>
+          Powered by{' '}
+          4ATech<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      </footer>
+    </div>
+  )
+}
