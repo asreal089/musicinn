dcf692801c5a75c39d60345f81cd3537e7249dd1
asreal089 <lexx_89@hotmail.com>
2020-11-29 20:50:43 -0300

    add bootstrap e primeiro componente

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import {Nav} from 'react-bootstrap'
+
+
+export default function Navbar() {
+    return(
+        <div>
+           <Nav
+              activeKey="/home"
+              className="justify-content-left"
+              onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+            >
+              <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                  Disabled
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+        </div>
+
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>MusicINN - a casa de shows online</title>
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
-
+      <Navbar />
       <main className={styles.main}>
         <h1> olar nosso sistema de musicas</h1>
       </main>
