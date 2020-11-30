5704b3d140d1150254901ac10103f4182e926cce
asreal089 <lexx_89@hotmail.com>
2020-11-30 00:55:58 -0300

    Ajustes em paginas

--- pages/musicians.js
+++ pages/musicians.js
@@ -1,9 +1,29 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import MyNavbar from '../components/MyNavbar'
+
 export default function musicians() {
     return(
+        <div className={styles.main_container}>
 
-        <div>
-            <h2>olar essa é a pagina de musicos:</h2>
+          <MyNavbar />
+          <div className={styles.container} >
+            <Head>
+              <title>MusicINN - a casa de shows online</title>
+              <link rel="icon" href="/favicon.ico" />
+              
+            </Head>
+            <main className={styles.main}>
+                <div>
+                    <h2>olar essa é a pagina de musicos</h2>
+                </div>
+            </main>
 
+            <footer className={styles.footer}>
+                Powered by{' '}
+                4ATech<img src="/4Atech.png" alt="4ATech Logo" className={styles.logo} />
+            </footer>
+          </div>
         </div>
     );
 }
