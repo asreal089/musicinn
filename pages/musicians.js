3634fd658eb16eb6d8ecdb6e056dad223128ccfc
deh410 <aearahawa@gmail.com>
2020-12-03 20:05:09 -0300

    minor bootstrap fixes

--- pages/musicians.js
+++ pages/musicians.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import MyNavbar from '../components/MyNavbar'
 import MyMusicianCard from '../components/MyMusicianCard'
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import fetch from 'isomorphic-unfetch';
 import axios from 'axios'
 
@@ -11,27 +11,28 @@ export default function musicians({allMusicos}) {
     return(
         <Container className={styles.main_container} >
           <MyNavbar />
-          <div className={styles.container} >
+          <Container className={styles.container} >
             <Head>
               <title>MusicINN - a casa de shows online</title>
               <link rel="icon" href="/favicon.ico" />
               
             </Head>
             <main className={styles.main}>
-                <h2>Esses são os músicos disponiveis no momento.</h2>
-                <div>
-                    { allMusicos.map((musico) => (
-                    <MyMusicianCard key={musico.musico_id} musico={musico}/>
-                    ))}
-                     
-                </div>
+              <Container fluid>
+                <h3 className="text-center" style={{marginTop: "32px"}}>Esses são os músicos disponiveis no momento:</h3>
+                <Row>
+                  { allMusicos.map((musico) => (
+                  <MyMusicianCard key={musico.musico_id} musico={musico}/>
+                  ))}
+                </Row>
+              </Container>
             </main>
 
             <footer className={styles.footer}>
                 Powered by{' '}
                 4ATech<img src="/4Atech.png" alt="4ATech Logo" className={styles.logo} />
             </footer>
-          </div>
+          </Container>
         </Container>
     );
 }
