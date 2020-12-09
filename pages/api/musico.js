7e4328fe85464b988690607a27d5fce38c699ad3
asreal089 <lexx_89@hotmail.com>
2020-12-09 05:30:35 -0300

    add rota de post musico

--- pages/api/musico.js
+++ pages/api/musico.js
@@ -2,10 +2,17 @@ import { connectToDatabase } from "../../util/mongodb";
 
 export default async function getMusicos(req, res){
 
+  
   const { db } = await connectToDatabase();
 
-  const musicos = await db.collection('musico').find({}).toArray();
   if(req.method =='GET'){    
+    const musicos = await db.collection('musico').find({}).toArray();
     res.json({musicos});      
+  }
+
+  if(req.method =='POST'){
+    const novoMusico = req.body;
+    db.collection('musico').insert(novoMusico);
+    res.json(novoMusico);
   }
 }
